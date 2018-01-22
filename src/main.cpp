ea62eb7f6a753d88cf79108044187d25da136278
jabbathepole <d.jablonski@hotmail.co.uk>
2018-01-22 15:11:40 +0000

    Added green/blue functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include <mbed.h>
 
+void on(DigitalOut LED) {
+  LED.write(0);
+}
+void off(DigitalOut LED) {
+  LED.write(1);
+}
+void flash(DigitalOut LED) {
+  on(LED);
+  wait(0.5);
+  off(LED);
+  wait(0.5);
+}
+
 int main() {
 
-    DigitalOut red(PTB22);
+    const DigitalOut RED(PTB22);
+    const DigitalOut GREEN(PTE26);
+    const DigitalOut BLUE(PTB21);
 
     while(1) {
-      red.write(0);
-      wait(0.5);
-      red.write(1);
-      wait(0.5);
+      flash(RED);
+      flash(GREEN);
+      flash(BLUE);
     }
 }
